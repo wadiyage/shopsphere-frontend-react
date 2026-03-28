86291346425053c69fd7586e41cbe43642ee0542
Sandaruwan Wadiyage <wadiyage567@gmail.com>
2026-03-28 20:58:15 +0530

    feat(navbar): add logo and improve layout styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom"
+import logo from "../assets/shopsphere.png"
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-900 text-white p-4 flex justify-between items-center">
-      <h1 className="text-xl font-bold">Shopsphere</h1>
+
+      <Link to="/" className="flex items-center gap-2">
+        <img src={logo} alt="ShopSphere Logo" className="w-12 h-12 object-contain" />
+        <span className="text-xl font-bold">Shopsphere</span>
+      </Link>
       <div className="space-x-4">
         <Link to="/" className="hover: text-gray-300">Home</Link>
         <Link to="/cart" className="hover:text-gray-300">Cart</Link>
